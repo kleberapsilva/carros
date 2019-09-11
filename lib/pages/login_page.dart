40af6e3f790a62f335a29b9a37245a26cc2fccfd
Kleber Silva <kleberapsilva@gmail.com>
2019-09-11 01:26:13 -0300

    initical

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      //color: Colors.white,
+    );
+  }
+}
